d516c45e4ece44c215f12aa12d1fe8ba6073cc58
Luis Miguel Gomez Londono <luismigolondo@gmail.com>
2019-12-02 16:51:28 -0500

    Init

---
+++ src/ProblemaA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Luis Miguel Gomez 201729597 , Nicolas Rugeles 201731740
+ *
+ */
+public class ProblemaA {
+
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		char eleccion = sc.nextLine().toUpperCase().charAt(0);
+		
+	}
+}
---
+++ src/ProblemaC.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Luis Miguel Gomez 201729597 , Nicolas Rugeles 201731740
+ *
+ */
+public class ProblemaC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
